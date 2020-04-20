4425ff6844ed48e6a7d59273becd9a25e1a31e20
Matheus Fidelis <matheus@nanoshots.com.br>
2020-04-20 09:57:06 -0300

    fix vpc subnet

--- modules/network/private.tf
+++ modules/network/private.tf
@@ -19,3 +19,13 @@ resource "aws_subnet" "private_subnet_1c" {
         Name = format("%s-private-1c", var.cluster_name)
     }
 }
+
+resource "aws_route_table_association" "private1a" {
+  subnet_id = aws_subnet.private_subnet_1a.id
+  route_table_id = aws_route_table.nat.id
+}
+
+resource "aws_route_table_association" "private1c" {
+  subnet_id = aws_subnet.private_subnet_1c.id
+  route_table_id = aws_route_table.nat.id
+}
